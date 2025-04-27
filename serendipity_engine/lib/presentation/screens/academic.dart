ad96fa1e19988e59722b88453baa70be8602ffe2
benicio-ayala58 <ayalabenicio85@gmail.com>
2025-04-26 20:35:05 -0600

    quick changes

--- serendipity_engine/lib/presentation/screens/academic.dart
+++ serendipity_engine/lib/presentation/screens/academic.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'ptest.dart';
 
 class academic extends StatelessWidget {
   const academic({super.key});
--- serendipity_engine/lib/presentation/screens/create_account.dart
+++ serendipity_engine/lib/presentation/screens/create_account.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'ptest.dart';
 
 class MyWidget extends StatefulWidget {
   const MyWidget({super.key});
