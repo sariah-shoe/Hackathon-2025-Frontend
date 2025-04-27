5e85db370d30f8c63789a19b68571b8f15098702
sarshoem <sariah.shoemaker@du.edu>
2025-04-26 20:57:55 -0600

    Kind of made it work?

--- serendipity_engine/lib/presentation/screens/login.dart
+++ serendipity_engine/lib/presentation/screens/login.dart
@@ -1,8 +1,25 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'home.dart';
+import 'package:http/http.dart' as http;
+
+Future<http.Response> auth(username, password) async {
+  final response = await http.post(
+    Uri.parse(
+      'https://teaching-neutral-rattler.ngrok-free.app/api/auth/token/',
+    ),
+    headers: {HttpHeaders.contentTypeHeader: 'application/json'},
+    body: jsonEncode({'email': username, 'password': password}),
+  );
+
+  return (response);
+}
 
 // Define a custom Form widget.
 class Login extends StatefulWidget {
+
   const Login({super.key});
 
   @override
@@ -38,36 +55,45 @@ class LoginState extends State<Login> {
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
     return Scaffold(
-    body: Form (
-      key: _formKey,
-      child: Column(
-        children: [
-          // Add TextFormFields and ElevatedButton here.
-          Text("Username"),
-          TextFormField(controller: usernameController),
-          Text("Password"),
-          TextFormField(controller: passwordController),
-          ElevatedButton(onPressed: (){
-            debugPrint("Username: ${usernameController.text} Password: ${passwordController.text}");
-            // showDialog(
-            //   context: context,
-            //   builder: (context) {
-            //   return AlertDialog(
-            //     // Retrieve the text that the user has entered by using the
-            //     // TextEditingController.
-            //     content: Text("Username: ${usernameController.text} Password: ${passwordController.text}"),
-            //   );
-            //   },
-            // );
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const Home())
-            );
-          },
-          child: const Text("Submit"))
-        ],
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            // Add TextFormFields and ElevatedButton here.
+            Text("Username"),
+            TextFormField(controller: usernameController),
+            Text("Password"),
+            TextFormField(controller: passwordController),
+            ElevatedButton(
+              onPressed: () {
+                debugPrint(
+                  "Username: ${usernameController.text} Password: ${passwordController.text}",
+                );
+                http.Response result = auth(
+                  usernameController.text,
+                  passwordController.text,
+                );
+                debugPrint('Returned result ${result.statusCode}');
+                if (result.statusCode == 200) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Home()),
+                  );
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Username or password incorrect')),
+                  );
+                }
+              },
+              child: const Text("Submit"),
+            ),
+          ],
+        ),
       ),
-    )
     );
   }
+}
+
+extension on Future<http.Response> {
+  get statusCode => null;
 }
