351a4e98a0a1f47c312fb7e60d266a354988ef8c
benicio-ayala58 <ayalabenicio85@gmail.com>
2025-04-26 18:24:43 -0600

    updating the registration page

--- serendipity_engine/lib/presentation/screens/register.dart
+++ serendipity_engine/lib/presentation/screens/register.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'academic.dart';
+import 'create_account.dart';
+
+
 
 class Register extends StatelessWidget {
   const Register({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Create an account',
+          style: const TextStyle(color: Colors.amber),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 7),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push
+              }
+              )
+          ],
+        ),
+      ),
+    );
   }
 }
