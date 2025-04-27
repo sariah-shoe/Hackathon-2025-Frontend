6a611aa145fb994a80869454eded744a79e49264
sarshoem <sariah.shoemaker@du.edu>
2025-04-26 18:24:07 -0600

    Completed login

--- serendipity_engine/lib/presentation/screens/login.dart
+++ serendipity_engine/lib/presentation/screens/login.dart
@@ -1,16 +1,73 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
-class Login extends StatelessWidget {
+// Define a custom Form widget.
+class Login extends StatefulWidget {
   const Login({super.key});
 
   @override
+  LoginState createState() {
+    return LoginState();
+  }
+}
+
+// Define a corresponding State class.
+// This class holds data related to the form.
+class LoginState extends State<Login> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<LoginState>.
+  final _formKey = GlobalKey<FormState>();
+
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Welcome!', style: const TextStyle(color: Colors.red)),
-        centerTitle: true,
-        backgroundColor: Colors.black,
+    body: Form (
+      key: _formKey,
+      child: Column(
+        children: [
+          // Add TextFormFields and ElevatedButton here.
+          Text("Username"),
+          TextFormField(controller: usernameController),
+          Text("Password"),
+          TextFormField(controller: passwordController),
+          ElevatedButton(onPressed: (){
+            debugPrint("Username: ${usernameController.text} Password: ${passwordController.text}");
+            // showDialog(
+            //   context: context,
+            //   builder: (context) {
+            //   return AlertDialog(
+            //     // Retrieve the text that the user has entered by using the
+            //     // TextEditingController.
+            //     content: Text("Username: ${usernameController.text} Password: ${passwordController.text}"),
+            //   );
+            //   },
+            // );
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const Home())
+            );
+          },
+          child: const Text("Submit"))
+        ],
       ),
+    )
     );
   }
 }
